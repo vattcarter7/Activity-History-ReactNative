0f512994db6f02f3c5ee1ecc72c8908bf3ea5d24
vattcarter7 <vattcarter@gmail.com>
2020-07-16 13:48:27 +0700

    removed constructor

--- src/components/Home/HomeView.js
+++ src/components/Home/HomeView.js
@@ -6,12 +6,16 @@ import i18n from '../../i18n/i18n';
 import StopWatchButton from '../StopWatchButton/StopWatchButton';
 
 class HomeView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: 0,
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     time: 0,
+  //   };
+  // }
+
+  state = {
+    time: 0,
+  };
 
   handleAppStateChange = async (nextAppState) => {
     const now = new Date().getTime();
